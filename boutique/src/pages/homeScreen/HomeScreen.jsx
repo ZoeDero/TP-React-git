56291a8b1002e26d7982880b8821ac07fedb2a2b
ZoeDero <zoe.dero@gmail.com>
2022-09-02 10:18:12 +0200

    E

---
+++ boutique/src/pages/homeScreen/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>La boutique de DEROZIER Zoé</h1>
+        </div>
+    );
+};
+
+export default HomeScreen;
